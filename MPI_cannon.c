7c179321423dd5657b417003bbdc25af42964d09
weiHelloWorld <wei.herbert.chen@gmail.com>
2015-11-17 15:52:48 -0600

    update

--- MPI_cannon.c
+++ MPI_cannon.c
@@ -9,6 +9,20 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_of_tasks); 
 
+	
+	int num_of_rows_and_cols_of_procs[2];  // currently we need to assume that both sizes of A and B are divisible by this
+
+	double **A, **B; // two input matrices
+	int size_of_matrix_A[2], size_of_matrix_B[2];
+
+	double **A_block, **B_block, **C_block; 
+
+	// for rank 0: get the input of the two matrices
+	
+
+
+
+
 	// initialization, partition into blocks and send to destination process
 
 	// shifting and calculate
