6739e35709bf0eb8b7d6c33a1f2ef495359ed5ae
weiHelloWorld <wei.herbert.chen@gmail.com>
2015-11-19 20:45:11 -0600

    refactor

--- MPI_cannon.c
+++ MPI_cannon.c
@@ -14,10 +14,10 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_of_procs); 
 
-    int num_of_rows_and_cols_of_procs; // num of rows should be equal to num of cols
+    int row_num_of_procs; // num of rows should be equal to num of cols
 
     double **A, **B; // two input matrices, matrices could be non-squared
-    int size_of_matrix_A[2], size_of_matrix_B[2];
+    int size_of_A[2], size_of_B[2];
     int size_of_A_block[2], size_of_B_block[2];
 
     #ifdef DEBUG
@@ -25,19 +25,19 @@ int main(int argc, char* argv[]) {
     B = create_matrix(9, 9);
     init_spec(A, 9, 9);
     init_spec(B, 9, 9);
-    size_of_matrix_A[0] = 9; size_of_matrix_A[1] = 9;
-    size_of_matrix_B[0] = 9; size_of_matrix_B[1] = 9;
-    num_of_rows_and_cols_of_procs = 3;
+    size_of_A[0] = 9; size_of_A[1] = 9;
+    size_of_B[0] = 9; size_of_B[1] = 9;
+    row_num_of_procs = 3;
     if (my_rank == 0) {
         print_matrix(A, 9, 9);
     }
 
     #endif
 
-    size_of_A_block[0] = size_of_matrix_A[0] / num_of_rows_and_cols_of_procs;
-    size_of_A_block[1] = size_of_matrix_A[1] / num_of_rows_and_cols_of_procs;
-    size_of_B_block[0] = size_of_matrix_B[0] / num_of_rows_and_cols_of_procs;
-    size_of_B_block[1] = size_of_matrix_B[1] / num_of_rows_and_cols_of_procs;
+    size_of_A_block[0] = size_of_A[0] / row_num_of_procs;
+    size_of_A_block[1] = size_of_A[1] / row_num_of_procs;
+    size_of_B_block[0] = size_of_B[0] / row_num_of_procs;
+    size_of_B_block[1] = size_of_B[1] / row_num_of_procs;
 
     double **A_block, **B_block, **C_block, **temp_block_buffer_A, **temp_block_buffer_B; 
     A_block = create_matrix(size_of_A_block[0], size_of_A_block[1]);
@@ -53,8 +53,8 @@ int main(int argc, char* argv[]) {
 
     // initialization, partition into blocks and send to destination process
     if (my_rank == 0) {
-        for (int i = 0; i < num_of_rows_and_cols_of_procs; i ++) {
-            for (int j = 0; j < num_of_rows_and_cols_of_procs; j ++) {
+        for (int i = 0; i < row_num_of_procs; i ++) {
+            for (int j = 0; j < row_num_of_procs; j ++) {
                 if (i == 0 && j == 0) { // copy to itself
                     for (int ii = 0; ii < size_of_A_block[0]; ii ++) {
                         for (int jj = 0; jj < size_of_A_block[1]; jj ++) {
@@ -69,7 +69,7 @@ int main(int argc, char* argv[]) {
                 }
                 else { 
                     // first send A block to processor 
-                    // (i, (j - i + num_of_rows_and_cols_of_procs) % num_of_rows_and_cols_of_procs)
+                    // (i, (j - i + row_num_of_procs) % row_num_of_procs)
                     for (int ii = i * size_of_A_block[0]; ii < (i+1) * size_of_A_block[0]; ii ++) {
                         for (int jj = j * size_of_A_block[1]; jj < (j+1) * size_of_A_block[1]; jj ++) {
                             // copy to a temp buffer before sending out
@@ -79,22 +79,20 @@ int main(int argc, char* argv[]) {
                     MPI_Send(&(temp_block_buffer_A[0][0]), 
                         size_of_A_block[0] * size_of_A_block[1], 
                         MPI_DOUBLE, 
-                        i * num_of_rows_and_cols_of_procs + ((j - i + num_of_rows_and_cols_of_procs) % num_of_rows_and_cols_of_procs),
+                        i * row_num_of_procs + ((j - i + row_num_of_procs) % row_num_of_procs),
                         0, /* tag = 0 is for A_block */
                         MPI_COMM_WORLD
                         );
                     #ifdef DEBUG
-                    if (i == 0 && j == 1) {
-                        printf("send to processor %d\n", i * num_of_rows_and_cols_of_procs + ((j - i + num_of_rows_and_cols_of_procs) % num_of_rows_and_cols_of_procs));
-                        printf("temp_block_buffer_A = \n");
-                        log_matrix(temp_block_buffer_A, 3, 3);
-                    }
+                    // printf("temp_block_buffer_A = \n");
+                    // log_matrix(temp_block_buffer_A, 3, 3);
+                    
                     assert(num_of_procs == 9);
                     #endif
 
                     assert(size_of_A_block[0] * size_of_A_block[1] == num_of_procs);
                     // secondly, send B block to procesor
-                    // ((i - j + num_of_rows_and_cols_of_procs) % num_of_rows_and_cols_of_procs, j)
+                    // ((i - j + row_num_of_procs) % row_num_of_procs, j)
                     for (int ii = i * size_of_B_block[0]; ii < (i+1) * size_of_B_block[0]; ii ++) {
                         for (int jj = j * size_of_B_block[1]; jj < (j+1) * size_of_B_block[1]; jj ++) {
                             temp_block_buffer_B[ii - i * size_of_B_block[0]][jj - j * size_of_B_block[1]] = B[ii][jj];
@@ -103,7 +101,7 @@ int main(int argc, char* argv[]) {
                     MPI_Send(temp_block_buffer_B[0], 
                         size_of_B_block[0] * size_of_B_block[1], 
                         MPI_DOUBLE, 
-                        ((i - j + num_of_rows_and_cols_of_procs) % num_of_rows_and_cols_of_procs) * num_of_rows_and_cols_of_procs + j,
+                        ((i - j + row_num_of_procs) % row_num_of_procs) * row_num_of_procs + j,
                         1, /* tag = 1 is for B_block */
                         MPI_COMM_WORLD
                         );
@@ -129,10 +127,8 @@ int main(int argc, char* argv[]) {
     // initialization ends here
     
     #ifdef DEBUG
-    if (my_rank == 1) {
         printf("the A block from rank %d is: \n", my_rank);
         log_matrix(A_block, 3,3);
-    }
     #endif
 
 
